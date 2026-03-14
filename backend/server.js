16c1bcc1e6dabf8e50858372e1e7f8bebef30149
boopathi2706 <boopathi.v2023cse@sece.ac.in>
2026-03-14 09:52:30 +0530

    add all deployed urls add in env

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,23 @@ const authRoutes = require('./routes/authRoutes');
 const budgetRoutes = require('./routes/budgetRoutes');
 
 const app=express();
-app.use(cors());
+const allowedOrigins = [
+  process.env.LOCAL_FRONTEND_URL,
+  process.env.PRODUCTION_FRONTEND_URL
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  })
+);
 app.use(express.json());
 
 app.use('/api/budget', budgetRoutes);
